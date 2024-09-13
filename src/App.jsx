9a3848fae63c796ce6441459e09742a4a0dce759
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-13 17:33:49 +0300

    Add Routes for login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
+import Login from './pages/users/Login';
+import Register from './pages/users/Register';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}></Route>
+        <Route path='/' element={<Layout />}>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
