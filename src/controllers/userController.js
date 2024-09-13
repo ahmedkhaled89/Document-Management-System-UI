8f5ba2a448f06f47fc8d2dbe8aaaa4aea8de0f7e
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-13 17:32:57 +0300

    Add registerUser Controller Function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,4 +19,45 @@ const loginUser = async (email, password) => {
   return data;
 };
 
-export { loginUser };
+// ************************ Login User ************************
+const registerUser = async ({
+  firstName,
+  lastName,
+  nationalID,
+  email,
+  password,
+  passwordConfirm,
+}) => {
+  if (
+    !firstName.trim() ||
+    !lastName.trim() ||
+    !nationalID.trim() ||
+    !password.trim() ||
+    !passwordConfirm.trim()
+  ) {
+    throw Error('ALL FIELDS ARE REQUIRED');
+  }
+  if (password !== passwordConfirm) {
+    throw Error('PASSWORD DOES NOT MATCH');
+  }
+  const res = await fetch('/api/users/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'Application/json',
+    },
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      nationalID,
+      email,
+      password,
+    }),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw Error(data.error);
+  }
+  return data;
+};
+export { loginUser, registerUser };
