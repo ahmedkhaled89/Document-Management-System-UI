1b3375faf4b10a0acb9740799ac8e34d53bed992
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-14 17:54:37 +0300

    Render Workspaces in Dashboard

--- src/pages/users/Dashboard.jsx
+++ src/pages/users/Dashboard.jsx
@@ -1,7 +1,49 @@
+import { useContext, useEffect, useState } from 'react';
+import { getAllWorkspaces } from '../../controllers/workspaceController';
+import { WorkspacesContext } from '../../contexts/WorkspacesContext';
+import Alert from '../../Components/Alert';
+import Workspace from '../../Components/Workspace';
+
 const Dashboard = () => {
+  // Error State
+  const [error, setError] = useState(null);
+
+  // Use Workspaces State
+  const { workspaces, setWorkspaces } = useContext(WorkspacesContext);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // Get All Workspaces
+        const data = await getAllWorkspaces();
+        // Update Workspaces state
+        setWorkspaces(data.workspaces);
+      } catch (error) {
+        setError(error);
+      }
+    })();
+    // setTimeout(
+    //   async () => {
+    //   try {
+    //     // Get All Workspaces
+    //     const data = await getAllWorkspaces();
+    //     // Update Workspaces state
+    //     setWorkspaces(data.workspaces);
+    //   } catch (error) {
+    //     setError(error);
+    //   }
+    // }, 0);
+  }, []);
+
   return (
     <section className='card'>
       <h1 className='title'>Dashboard</h1>
+
+      {workspaces &&
+        workspaces.map((workspace) => (
+          <Workspace key={workspace._id} workspace={workspace} />
+        ))}
+      {error && <Alert message={error} />}
     </section>
   );
 };
