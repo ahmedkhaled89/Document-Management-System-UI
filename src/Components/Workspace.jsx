719bfea17db5dd99b8328c4c11748d4df82372a7
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-14 22:45:31 +0300

    Update Workspace Component Styles

--- src/Components/Workspace.jsx
+++ src/Components/Workspace.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable react/prop-types */
 const Workspace = ({ workspace }) => {
   return (
-    <div className='card bg-red-400 mb-5'>
-      <p>{workspace.name}</p>
-      <p>{workspace.ownerID.email}</p>
+    <div className='card bg-indigo-500 text-white px-4 py-2  mb-2'>
+      <h2
+        title='Workspace Name'
+        className='font-bold text-lg first-letter:uppercase mb-[-6px]'
+      >
+        {workspace.name}
+      </h2>
+      <p className='text-[10px] mb-4'>
+        <span title='Owner Name'>
+          {`${workspace.ownerID.firstName} ${workspace.ownerID.lastName} `}
+        </span>
+        |<span title='Owner NID'> {workspace.ownerID.nationalID} </span> |
+        <span title='Owner Email'> {workspace.ownerID.email}</span>
+      </p>
     </div>
   );
 };
