5826c93cde092ec13d0e9eb7e7ef9dcea7f7f89b
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-14 16:25:55 +0300

    Wrap App wit WorkspacesProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './assets/app.css';
 import UserProvider from './contexts/UserContext.jsx';
+import WorkspacesProvider from './contexts/WorkspacesContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
-      <App />
+      <WorkspacesProvider>
+        <App />
+      </WorkspacesProvider>
     </UserProvider>
   </StrictMode>
 );
