05e33ba77f80f1f292e1733eb5573962be1c72cd
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-15 08:28:00 +0300

    Add Download Functionality to Doc Component

--- src/Components/Doc.jsx
+++ src/Components/Doc.jsx
@@ -1,12 +1,30 @@
 import { Link } from 'react-router-dom';
+import { downloadDoc } from '../controllers/docController';
 
 /* eslint-disable react/prop-types */
 const Doc = ({ doc }) => {
+  // Grab Doc Name and id
+  const _id = doc._id;
+  const fileName = doc.docName;
+
+  // Handle Download
+  const handleDownload = async () => {
+    try {
+      await downloadDoc(_id, fileName);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='card font-bold mb-2 p-2 flex justify-between'>
       <h3>{doc.docName}</h3>
       <div className='flex gap-2'>
-        <Link title='Download' className='fa-solid fa-download'></Link>
+        <Link
+          title='Download'
+          className='fa-solid fa-download'
+          onClick={handleDownload}
+        ></Link>
         <Link title='Preview' className='fa-solid fa-eye'></Link>
         {doc.ownerID == localStorage.getItem('_id') && (
           <>
