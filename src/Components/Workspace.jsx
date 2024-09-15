f8bcbbdad453f88598d138d12cd740bc2a641452
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-15 21:51:28 +0300

    Add Upload Doc Icon

--- src/Components/Workspace.jsx
+++ src/Components/Workspace.jsx
@@ -4,20 +4,29 @@ import Doc from './Doc';
 const Workspace = ({ workspace }) => {
   return (
     <div className='card mb-6 bg-slate-200'>
-      <div>
-        <h2
-          title='Workspace Name'
-          className='font-bold text-lg first-letter:uppercase mb-[-6px]'
-        >
-          {workspace.name}
-        </h2>
-        <p className='text-[10px] mb-4'>
-          <span title='Owner Name'>
-            {`${workspace.ownerID.firstName} ${workspace.ownerID.lastName} `}
-          </span>
-          |<span title='Owner NID'> {workspace.ownerID.nationalID} </span> |
-          <span title='Owner Email'> {workspace.ownerID.email}</span>
-        </p>
+      <div className='flex justify-between'>
+        <div>
+          <h2
+            title='Workspace Name'
+            className='font-bold text-lg first-letter:uppercase mb-[-6px]'
+          >
+            {workspace.name}
+          </h2>
+          <p className='text-[10px] mb-4'>
+            <span title='Owner Name'>
+              {`${workspace.ownerID.firstName} ${workspace.ownerID.lastName} `}
+            </span>
+            |<span title='Owner NID'> {workspace.ownerID.nationalID} </span> |
+            <span title='Owner Email'> {workspace.ownerID.email}</span>
+          </p>
+        </div>
+        <div>
+          <i
+            to={`upload`}
+            title='Add New Document'
+            className='fa-solid fa-plus'
+          ></i>
+        </div>
       </div>
 
       <div>
