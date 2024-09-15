addf91c663fc89a6849c29cbcc07f64c8eedf656
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-15 08:26:22 +0300

    Change Workspace Component styles

--- src/Components/Workspace.jsx
+++ src/Components/Workspace.jsx
@@ -3,20 +3,23 @@ import Doc from './Doc';
 /* eslint-disable react/prop-types */
 const Workspace = ({ workspace }) => {
   return (
-    <div className='card bg-indigo-500 text-white px-4 py-2  mb-2'>
-      <h2
-        title='Workspace Name'
-        className='font-bold text-lg first-letter:uppercase mb-[-6px]'
-      >
-        {workspace.name}
-      </h2>
-      <p className='text-[10px] mb-4'>
-        <span title='Owner Name'>
-          {`${workspace.ownerID.firstName} ${workspace.ownerID.lastName} `}
-        </span>
-        |<span title='Owner NID'> {workspace.ownerID.nationalID} </span> |
-        <span title='Owner Email'> {workspace.ownerID.email}</span>
-      </p>
+    <div className='card mb-6 bg-slate-200'>
+      <div>
+        <h2
+          title='Workspace Name'
+          className='font-bold text-lg first-letter:uppercase mb-[-6px]'
+        >
+          {workspace.name}
+        </h2>
+        <p className='text-[10px] mb-4'>
+          <span title='Owner Name'>
+            {`${workspace.ownerID.firstName} ${workspace.ownerID.lastName} `}
+          </span>
+          |<span title='Owner NID'> {workspace.ownerID.nationalID} </span> |
+          <span title='Owner Email'> {workspace.ownerID.email}</span>
+        </p>
+      </div>
+
       <div>
         {workspace.DocsIDs &&
           workspace.DocsIDs.map((d) => <Doc key={d._id} doc={d} />)}
