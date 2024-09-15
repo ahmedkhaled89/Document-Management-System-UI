20c06a5d50ba8111af34f780c89a65c34d632c5b
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-15 18:35:28 +0300

    Handle Document Deletion & change some styles

--- src/Components/Doc.jsx
+++ src/Components/Doc.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
-import { downloadDoc } from '../controllers/docController';
+import { deleteDoc, downloadDoc } from '../controllers/docController';
+import { useState } from 'react';
 
 /* eslint-disable react/prop-types */
 const Doc = ({ doc }) => {
+  // Delete State
+  const [deleted, setDeleted] = useState(false);
+
   // Grab Doc Name and id
   const _id = doc._id;
   const fileName = doc.docName;
@@ -16,23 +20,46 @@ const Doc = ({ doc }) => {
     }
   };
 
+  // Handle Delete
+  const handleDelete = async () => {
+    try {
+      const data = await deleteDoc(_id);
+      console.log(data);
+      setDeleted(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='card font-bold mb-2 p-2 flex justify-between'>
-      <h3>{doc.docName}</h3>
-      <div className='flex gap-2'>
-        <Link
-          title='Download'
-          className='fa-solid fa-download'
-          onClick={handleDownload}
-        ></Link>
-        <Link title='Preview' className='fa-solid fa-eye'></Link>
-        {doc.ownerID == localStorage.getItem('_id') && (
-          <>
-            <Link title='Update' className='fa-solid fa-pen-to-square'></Link>
-            <Link title='Delete' className='fa-solid fa-trash'></Link>
-          </>
-        )}
-      </div>
+    <div>
+      {!deleted && (
+        <div className='card font-bold mb-2 p-2 flex justify-between'>
+          <h3>{doc.docName}</h3>
+          <div className='flex gap-2'>
+            <Link
+              title='Download'
+              className='fa-solid fa-download'
+              onClick={handleDownload}
+            ></Link>
+            <Link title='Preview' className='fa-solid fa-eye'></Link>
+            {doc.ownerID._id === localStorage.getItem('_id') && (
+              <>
+                <Link
+                  title='Update'
+                  className='fa-solid fa-pen-to-square'
+                ></Link>
+                <Link
+                  title='Delete'
+                  className='fa-solid fa-trash'
+                  onClick={handleDelete}
+                ></Link>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+      {deleted && <div>Deleted Successfully</div>}
     </div>
   );
 };
