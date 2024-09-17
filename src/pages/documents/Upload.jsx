388d4617a10d0ae0a096b2496f16b2347508b1af
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-17 21:13:29 +0300

    Add Upload Page

---
+++ src/pages/documents/Upload.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Upload = () => {
+  // Get workspaceID from location
+  const location = useLocation();
+  const { workspaceID } = location.state;
+
+  // Navigate Hook
+  const navigate = useNavigate();
+
+  // File State
+  const [file, setFile] = useState();
+
+  // Set File state
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  // Handle Submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('doc', file);
+    formData.append('workspaceID', workspaceID);
+    const res = await fetch('/api/docs/upload', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: formData,
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      console.log(data.error);
+      throw Error(data.error);
+    }
+    console.log(data);
+    navigate('/dashboard');
+  };
+
+  return (
+    <section className='card'>
+      <h2>Upload New Document</h2>
+      <form onSubmit={handleSubmit}>
+        <h1>React File Upload</h1>
+        <input type='file' onChange={handleChange} />
+        <button className='btn mt-1 w-auto' type='submit'>
+          Upload
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Upload;
