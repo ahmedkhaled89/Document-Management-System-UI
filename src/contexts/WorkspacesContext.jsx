1692a3eea715b30b0332de8a09ec093969a0176d
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-21 19:12:07 +0300

    Add getAllWorkspaces Functionality to WorkspacesContext

--- src/contexts/WorkspacesContext.jsx
+++ src/contexts/WorkspacesContext.jsx
@@ -1,13 +1,33 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { getAllWorkspaces } from '../controllers/workspaceController';
 
 export const WorkspacesContext = createContext();
 
 const WorkspacesProvider = ({ children }) => {
+  // Workspace Status
   const [workspaces, setWorkspaces] = useState([]);
 
+  // Error Status
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // Get All Workspaces
+        const data = await getAllWorkspaces();
+        // Update Workspaces state
+        setWorkspaces(data.workspaces);
+      } catch (error) {
+        setError(error);
+      }
+    })();
+  }, []);
+
   return (
-    <WorkspacesContext.Provider value={{ workspaces, setWorkspaces }}>
+    <WorkspacesContext.Provider
+      value={{ workspaces, setWorkspaces, error, setError }}
+    >
       {children}
     </WorkspacesContext.Provider>
   );
