292c77340ac8945eacfa405e28c1a30efdf1ae5b
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-25 11:20:38 +0300

    Add styles to Dashboard

--- src/pages/users/Dashboard.jsx
+++ src/pages/users/Dashboard.jsx
@@ -4,23 +4,22 @@ import Alert from '../../Components/Alert';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
-  // Use Workspaces State to get Workspaces or error
   const { workspaces, error } = useContext(WorkspacesContext);
 
   return (
-    <section className='card'>
+    <section className='p-6 bg-gray-100 min-h-screen'>
       {error ? (
         <Alert message={error} />
       ) : (
-        <div className='flex items-center justify-between'>
-          <h1 className='title'>Dashboard</h1>
-          <div>
-            <Link
-              to='/workspace/new'
-              title='Add New Workspace'
-              className='fa-solid fa-plus'
-            ></Link>
-          </div>
+        <div className='flex items-center justify-between mb-4'>
+          <h1 className='text-3xl font-bold text-gray-800'>Dashboard</h1>
+          <Link
+            to='/workspace/new'
+            title='Add New Workspace'
+            className='flex items-center justify-center w-10 h-10 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition duration-200'
+          >
+            <i className='fa-solid fa-plus'></i>
+          </Link>
         </div>
       )}
 
@@ -28,25 +27,25 @@ const Dashboard = () => {
         workspaces.map((workspace) => (
           <Link key={workspace._id} to={`/workspace/${workspace._id}`}>
             <div
-              className={`card mb-2 p-2 text-white ${
+              className={`mb-4 p-4 rounded-lg shadow-md transition-transform transform hover:scale-105 ${
                 workspace.ownerID._id === localStorage.getItem('_id')
-                  ? 'bg-slate-700'
-                  : 'bg-indigo-500'
+                  ? 'bg-slate-700 text-white'
+                  : 'bg-indigo-500 text-white'
               }`}
             >
-              <h2 className='title mb-0'>{workspace.name}</h2>
-              <p className='text-[10px] mb-4'>
+              <h2 className='text-xl font-semibold mb-1'>{workspace.name}</h2>
+              <p className='text-sm'>
                 <span title='Owner Name'>
                   {`${workspace.ownerID.firstName} ${workspace.ownerID.lastName} `}
                 </span>
-                |<span title='Owner NID'> {workspace.ownerID.nationalID} </span>
-                |<span title='Owner Email'> {workspace.ownerID.email}</span>
+                | <span title='Owner NID'>{workspace.ownerID.nationalID} </span>
+                | <span title='Owner Email'>{workspace.ownerID.email}</span>
               </p>
             </div>
           </Link>
         ))
       ) : (
-        <div className='title text-center text-indigo-500'>
+        <div className='text-center text-gray-500 text-lg'>
           No Workspaces Yet!
         </div>
       )}
